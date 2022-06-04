269141301a2d7452374ae35cf56e156410dd657a
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2022-06-04 23:17:33 +0200

    Check in lib

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+export declare const Cups: (milliliters: number) => string;
+export declare const Tablespoons: (milliliters: number) => string;
+export declare const Teaspoons: (milliliters: number) => string;
